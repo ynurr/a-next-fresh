c79473f58ee844bdbba77772dc1cac279e42a76f
ynurr <0305_08@naver.com>
2024-07-23 23:57:29 +0900

    페이지 레이아웃 만들기 (React 기초 문법)

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
 export default function Home() {
+  let name = 'park';
+  let link = 'https://naver.com';
   return (
     <div>
-      
+      <h4 className="title" style={{ color:'red', fontSize:'30px' }}>애플후레시</h4>
+      <p className="title-sub">by dev {name}</p>
+      <a href={link}>링크</a>
     </div>
   );
 }
