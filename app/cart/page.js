40c20855e50c12bf931f0db2bcbbef5fd8a61775
ynurr <0305_08@naver.com>
2024-08-12 23:03:25 +0900

    client/server component, import 문법

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,22 @@
+import {age, name} from './data.js'
+
 export default function Cart() {
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem/>
+      <CartItem/>
+      <CartItem/>
     </div>
   )
 } 
+
+function CartItem() {
+  return (
+    <div className="cart-item">
+      <p>상품명 {age}</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  )
+}
