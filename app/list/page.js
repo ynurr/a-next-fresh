897d4f710d3a48a0a79bc119b367c9e8ad35a5aa
ynurr <0305_08@naver.com>
2024-08-17 18:54:08 +0900

    버튼 만들기 (useState, onClick)

--- app/list/page.js
+++ app/list/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image"
+'use client'
+
+import { useState } from "react"
 
 export default function List() {
   let 상품 = ['Tomatoes','Pasta','Coconut']
-  let 어레이 = [2,3,4]
-  어레이.map(function(a,i){
-
-  })
+  let [수량, 수량변경] = useState(0)
 
   return (
     <div>
@@ -16,6 +15,8 @@ export default function List() {
             <div className="food" key={i}>
               <img src={`/food${i}.png`} className="food-img" />
               <h4>{a} $40</h4>
+              <span> {수량} </span>
+              <button onClick={()=>{ 수량변경(수량+1) }}>+</button>
             </div>
           )
         })
