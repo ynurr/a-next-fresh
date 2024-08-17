b16c96a3ad70162cca7ead2fce193af198fbda1a
ynurr <0305_08@naver.com>
2024-08-17 19:04:56 +0900

    버튼 만들기 (array, object state 변경)

--- app/list/page.js
+++ app/list/page.js
@@ -4,7 +4,7 @@ import { useState } from "react"
 
 export default function List() {
   let 상품 = ['Tomatoes','Pasta','Coconut']
-  let [수량, 수량변경] = useState(0)
+  let [수량, 수량변경] = useState([0, 0, 0])
 
   return (
     <div>
@@ -15,8 +15,17 @@ export default function List() {
             <div className="food" key={i}>
               <img src={`/food${i}.png`} className="food-img" />
               <h4>{a} $40</h4>
-              <span> {수량} </span>
-              <button onClick={()=>{ 수량변경(수량+1) }}>+</button>
+              <button onClick={()=>{
+                let copy = [...수량]
+                copy[i]--
+                수량변경(copy)
+              }}>-</button>
+              <span> {수량[i]} </span>
+              <button onClick={()=>{
+                let copy = [...수량]
+                copy[i]++
+                수량변경(copy)
+              }}>+</button>
             </div>
           )
         })
